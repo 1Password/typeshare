2d08b07f2c6fda23b981f00ae4b2d24597d97365
Jonathan Duran <jonathan.duran@agilebits.com>
2024-05-01 13:44:50 -0400

    Add Kotlin test output file

---
+++ core/data/tests/struct_decorator/output.kt
@@ -0,0 +1,37 @@
+package com.agilebits.onepassword
+
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.SerialName
+
+@Serializable
+data class BestHockeyTeams (
+	val PittsburghPenguins: UInt,
+	val Lies: String
+)
+
+@Serializable
+data class BestHockeyTeams1 (
+	val PittsburghPenguins: UInt,
+	val Lies: String
+)
+
+@Serializable
+data class BestHockeyTeams2 (
+	val PittsburghPenguins: UInt,
+	val Lies: String
+)
+
+@Serializable
+data class BestHockeyTeams3 (
+	val PittsburghPenguins: UInt,
+	val Lies: String
+) {
+	override fun toString(): String = "BestHockeyTeams3"
+}
+
+@Serializable
+data class BestHockeyTeams4 (
+	val PittsburghPenguins: UInt,
+	val Lies: String
+)
+
