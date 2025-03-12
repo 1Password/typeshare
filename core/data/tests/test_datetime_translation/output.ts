a7accd943fe87d7a4b0c3699171429b351591b04
Omar Miraj <omar.miraj@agilebits.com>
2025-03-12 11:59:20 -0400

    update typescript update

--- core/data/tests/test_datetime_translation/output.ts
+++ core/data/tests/test_datetime_translation/output.ts
@@ -3,16 +3,22 @@ export interface Foo {
 	time2: Date;
 }
 
-export function ReviverFunc(key: string, value: unknown): unknown {
+/**
+ * Custom JSON reviver and replacer functions for dynamic data transformation
+ * ReviverFunc is used during JSON parsing to detect and transform specific data structures
+ * ReplacerFunc is used during JSON serialization to modify certain values before stringifying.
+ * These functions allow for flexible encoding and decoding of data, ensuring that complex types are properly handled when converting between TS objects and JSON
+ */
+export const ReviverFunc = (key: string, value: unknown): unknown => {
     if (/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$/.test(value as string)) {
         return new Date(value as string)
     }
     return value;
-}
+};
 
-export function ReplacerFunc(key: string, value: unknown): unknown {
+export const ReplacerFunc = (key: string, value: unknown): unknown => {
     if (value instanceof Date) {
         return value.toISOString();
     }
     return value;
-}
+};
