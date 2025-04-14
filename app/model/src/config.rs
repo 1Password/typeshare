49d47dab3f9ccc218850823c34b26ae3c3dbbf7d
Nathan West <nathan.west@agilebits.com>
2025-04-14 16:09:12 -0400

    Improve `Unconfigured` impl

--- app/model/src/config.rs
+++ app/model/src/config.rs
@@ -2,18 +2,57 @@
 Utilities for configuration for Language implementations
  */
 
-use serde::{de, ser::SerializeStruct};
+use serde::{de, ser};
 
 /// Languages that take no configuration can use this as their
 /// `Language::Config` type
 #[derive(Debug, Clone, Copy, Default)]
 pub struct Unconfigured;
 
-impl serde::Serialize for Unconfigured {
+struct DeadKey;
+
+impl<'de> de::Deserialize<'de> for DeadKey {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: de::Deserializer<'de>,
+    {
+        deserializer.deserialize_identifier({
+            struct Visitor;
+
+            impl<'de> de::Visitor<'de> for Visitor {
+                type Value = DeadKey;
+
+                fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+                    formatter.write_str("a struct key")
+                }
+
+                fn visit_str<E>(self, _v: &str) -> Result<Self::Value, E>
+                where
+                    E: de::Error,
+                {
+                    Ok(DeadKey)
+                }
+
+                fn visit_bytes<E>(self, _v: &[u8]) -> Result<Self::Value, E>
+                where
+                    E: de::Error,
+                {
+                    Ok(DeadKey)
+                }
+            }
+
+            Visitor
+        })
+    }
+}
+
+impl ser::Serialize for Unconfigured {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: serde::Serializer,
     {
+        use ser::SerializeStruct as _;
+
         serializer.serialize_struct("Unconfigured", 0)?.end()
     }
 }
@@ -23,23 +62,26 @@ impl<'de> serde::Deserialize<'de> for Unconfigured {
     where
         D: serde::Deserializer<'de>,
     {
-        struct Visitor;
+        deserializer.deserialize_struct("Unconfigured", &[], {
+            struct Visitor;
 
-        impl<'de> de::Visitor<'de> for Visitor {
-            type Value = Unconfigured;
+            impl<'de> de::Visitor<'de> for Visitor {
+                type Value = Unconfigured;
 
-            fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-                write!(formatter, "an empty struct")
+                fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+                    write!(formatter, "an empty struct")
+                }
+
+                fn visit_map<A>(self, mut map: A) -> Result<Self::Value, A::Error>
+                where
+                    A: de::MapAccess<'de>,
+                {
+                    while let Some((DeadKey, de::IgnoredAny)) = map.next_entry()? {}
+                    Ok(Unconfigured)
+                }
             }
 
-            fn visit_map<A>(self, mut map: A) -> Result<Self::Value, A::Error>
-            where
-                A: de::MapAccess<'de>,
-            {
-                while let Some((de::IgnoredAny, de::IgnoredAny)) = map.next_entry()? {}
-                Ok(Unconfigured)
-            }
-        }
-        deserializer.deserialize_struct("Unconfigured", &[], Visitor)
+            Visitor
+        })
     }
 }
