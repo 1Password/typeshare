e5a29c91d50127b123d539bd667bf429cf9401d4
Darrell Roberts <droberts0674@gmail.com>
2025-10-24 17:57:44 -0400

    add snapshot test

--- app/cli/snapshot-tests/test_cfg_attr_typeshare/input.rs
+++ app/cli/snapshot-tests/test_cfg_attr_typeshare/input.rs
@@ -1,4 +1,34 @@
+/// Example of a type that is conditionally typeshared
+/// based on a feature "typeshare-support". This does not
+/// conditionally typeshare but allows a conditionally
+/// typeshared type to generate typeshare types when behind
+/// a `cfg_attr` condition.
 #[cfg_attr(feature = "typeshare-support", typeshare)]
 pub struct TestStruct1 {
     field: String,
 }
+
+#[cfg_attr(feature = "typeshare-support", typeshare(transparent))]
+#[derive(Debug, Default, PartialEq, Eq, Clone, Hash)]
+#[repr(transparent)]
+pub struct Bytes(Vec<u8>);
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+#[cfg_attr(
+    feature = "typeshare-support",
+    typeshare(
+        swift = "Equatable, Hashable",
+        swiftGenericConstraints = "R: Equatable & Hashable"
+    )
+)]
+pub struct TestStruct2<R> {
+    field_1: String,
+    field_2: R,
+}
+
+#[cfg_attr(
+    feature = "typeshare-support",
+    typeshare(kotlin = "JvmInline", redacted)
+)]
+pub struct TestStruct3(String);
