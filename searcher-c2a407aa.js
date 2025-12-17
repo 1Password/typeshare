7f8bd25d16071f63ff62e55e25cb95f8a867ac17
darrell-roberts <33698065+darrell-roberts@users.noreply.github.com>
2025-12-17 22:39:27 +0000

    Deploying to gh-pages from @ 1Password/typeshare@0364ed83e619b012d2ad57c1eb5274b44e0bdd01 🚀

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-15e65a48.js',
+                path_to_root + 'searchindex-da456943.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
